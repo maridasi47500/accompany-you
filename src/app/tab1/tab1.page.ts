c4a63d67f9dadc8e523940657ade310f05d9291f
anonyme hacker <anonyme201504@gmail.com>
2024-03-18 16:56:06 -0300

    hey

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,111 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
-})
-export class Tab1Page {
-
-  constructor() {}
-
+import { Component,OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
+import {ActivatedRoute, RouterModule} from '@angular/router';
+import {
+	  AngularFireStorage,
+	    AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
+import {
+	  AngularFirestore,
+	    AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+export interface imgFile {
+	  name: string;
+	    filepath: string;
+	      size: number;
 }
+@Component({
+	  selector: 'app-tab1',
+	    templateUrl: 'tab1.page.html',
+	      styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page implements OnInit {
+
+	// File upload task
+	   fileUploadTask: AngularFireUploadTask;
+	     // Upload progress
+	       percentageVal: Observable<any>;
+	         // Track file uploading with snapshot
+	           trackSnapshot: Observable<any>;
+	             // Uploaded File URL
+	               UploadedImageURL: Observable<string>;
+	                 // Uploaded image collection
+	                   files: Observable<imgFile[]>;
+	                     // Image specifications
+	                       imgName: string;
+	                         imgSize: number;
+	                           // File uploading status
+	                             isFileUploading: boolean;
+	                               isFileUploaded: boolean;
+	                                 private filesCollection: AngularFirestoreCollection<imgFile>;
+					    ngOnInit() {
+						            this.route.params.subscribe(params => {
+								                 console.log(params['brand']);
+										         });
+											     }
+	                                   constructor(
+	                                       private afs: AngularFirestore,
+	                                           private afStorage: AngularFireStorage,private route: ActivatedRoute
+	                                             ) {
+	                                                 this.isFileUploading = false;
+	                                                     this.isFileUploaded = false;
+	                                                         // Define uploaded files collection
+	                                                             this.filesCollection = afs.collection<imgFile>('imagesCollection');
+	                                                                 this.files = this.filesCollection.valueChanges();
+	                                                                   }
+	                                                                     uploadImage(event: FileList) {
+	                                                                         const file: any = event.item(0);
+	                                                                             // Image validation
+	                                                                                 if (file.type.split('/')[0] !== 'image') {
+	                                                                                       console.log('File type is not supported!');
+	                                                                                             return;
+	                                                                                                 }
+	                                                                                                     this.isFileUploading = true;
+	                                                                                                         this.isFileUploaded = false;
+	                                                                                                             this.imgName = file.name;
+	                                                                                                                 // Storage path
+	                                                                                                                     const fileStoragePath = `filesStorage/${new Date().getTime()}_${file.name}`;
+	                                                                                                                         // Image reference
+	                                                                                                                             const imageRef = this.afStorage.ref(fileStoragePath);
+	                                                                                                                                 // File upload task
+	                                                                                                                                     this.fileUploadTask = this.afStorage.upload(fileStoragePath, file);
+	                                                                                                                                         // Show uploading progress
+	                                                                                                                                             this.percentageVal = this.fileUploadTask.percentageChanges();
+	                                                                                                                                                 this.trackSnapshot = this.fileUploadTask.snapshotChanges().pipe(
+	                                                                                                                                                       finalize(() => {
+	                                                                                                                                                               // Retreive uploaded image storage path
+	                                                                                                                                                                       this.UploadedImageURL = imageRef.getDownloadURL();
+	                                                                                                                                                                               this.UploadedImageURL.subscribe(
+	                                                                                                                                                                                         (resp) => {
+	                                                                                                                                                                                                     this.storeFilesFirebase({
+	                                                                                                                                                                                                                   name: file.name,
+	                                                                                                                                                                                                                                 filepath: resp,
+	                                                                                                                                                                                                                                               size: this.imgSize,
+	                                                                                                                                                                                                                                                           });
+	                                                                                                                                                                                                                                                                       this.isFileUploading = false;
+	                                                                                                                                                                                                                                                                                   this.isFileUploaded = true;
+	                                                                                                                                                                                                                                                                                             },
+	                                                                                                                                                                                                                                                                                                       (error) => {
+	                                                                                                                                                                                                                                                                                                                   console.log(error);
+	                                                                                                                                                                                                                                                                                                                             }
+	                                                                                                                                                                                                                                                                                                                                     );
+	                                                                                                                                                                                                                                                                                                                                           }),
+	                                                                                                                                                                                                                                                                                                                                                 tap((snap: any) => {
+	                                                                                                                                                                                                                                                                                                                                                         this.imgSize = snap.totalBytes;
+	                                                                                                                                                                                                                                                                                                                                                               })
+	                                                                                                                                                                                                                                                                                                                                                                   );
+	                                                                                                                                                                                                                                                                                                                                                                     }
+	                                                                                                                                                                                                                                                                                                                                                                       storeFilesFirebase(image: imgFile) {
+	                                                                                                                                                                                                                                                                                                                                                                           const fileId = this.afs.createId();
+	                                                                                                                                                                                                                                                                                                                                                                               this.filesCollection
+	                                                                                                                                                                                                                                                                                                                                                                                     .doc(fileId)
+	                                                                                                                                                                                                                                                                                                                                                                                           .set(image)
+	                                                                                                                                                                                                                                                                                                                                                                                                 .then((res) => {
+	                                                                                                                                                                                                                                                                                                                                                                                                         console.log(res);
+	                                                                                                                                                                                                                                                                                                                                                                                                               })
+	                                                                                                                                                                                                                                                                                                                                                                                                                     .catch((err) => {
+	                                                                                                                                                                                                                                                                                                                                                                                                                             console.log(err);
+	                                                                                                                                                                                                                                                                                                                                                                                                                                   });
+	                                                                                                                                                                                                                                                                                                                                                                                                                                     }
+	                                                                                                                                                                                                                                                                                                                                                                                                                                     }
